9c24f3774b3d8211710de8a8c1a0557f403aac2a
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-15 11:30:47 -0800

    add constructor to back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,26 @@
+function Pizza(size, garnish){
+  this.size = size;
+  this.garnish = garnish;
+  this.total = 0;
+}
+
+
+
+
+
+
 //User Interface Logic
 $(document).ready(function(){
   $("form#order-option").submit(function(event){
     event.preventDefault();
     var size = $("#size").val();
+    var garnish = [];
     $("input:checkbox[name=toppings]:checked").each(function(){
-      debugger
-      var garnish = $(this).val();
-      $(".topping-result").append(garnish + "<br>");
+      garnish.push($(this).val());
     });
+    newPizza = new Pizza(size,garnish);
     $(".size-result").text(size);
+    $(".topping-result").text(garnish);
     $(".grandtotal").text(8.00);
   });
 });
