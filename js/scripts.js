e33ed310613fd01146dd2faabcf0a8f7481736de
Edgar Hernandez <edgarchortiz@gmail.com>
2017-12-15 10:51:20 -0800

    add div class and label in html

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
   $("form#order-option").submit(function(event){
     event.preventDefault();
     var size = $("#size").val();
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      debugger
+      var garnish = $(this).val();
+      $(".topping-result").append(garnish + "<br>");
+    });
     $(".size-result").text(size);
+    $(".grandtotal").text(8.00);
   });
 });
